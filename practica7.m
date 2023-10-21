a39927a4f5280e2a87d6c2c7f13bff23dab507d8
sabbaggh <137013454+sabbaggh@users.noreply.github.com>
2023-10-21 01:09:12 -0600

    Add files via upload

--- practica7.m
+++ practica7.m
@@ -6,19 +6,43 @@ a = imread('peppers.png');
 a = rgb2gray(a);
 [alto, ancho] = size(a);
 conBordes = zeros(alto+2,ancho+2,"uint8");
-conBordes(2:end-1,2:end-1) = a;
+ruidosa = imnoise(a,"salt & pepper",0.04);
+conBordes(2:end-1,2:end-1) = ruidosa;
+
+matrizRuido = detectPRuidoso(conBordes,alto,ancho);
+subplot(1,2,1)
+imshow(a)
+title('Original')
+subplot(1,2,2)
 imshow(conBordes)
+title('Noise')
 
-
+function porCentral = quitarRuidoPorCentral(matrizRuido)
+end
 
 function porMedia = calcMedia(imagenR,alto,ancho)
-    
-    [alto2, ancho2] = size(imagenR)
+    [alto2, ancho2] = size(imagenR);
     porMedia = zeros(alto2,ancho2,"uint8");
     for i =2:alto2
         for j=2:ancho2
-            porMedia(i,j)=round((double(imagenR(i,j)+double(imagenR(i+1,j+1))))
+            porMedia(i,j)=round((double(imagenR(i,j))+double(imagenR(i+1,j+1))+double(imagenR(i-1,j-1))+double(imagenR(i-1,j))+double(imagenR(i-1,j+1))+double(imagenR(i,j-1))+double(imagenR(i+1,j-1))+double(imagenR(i+1,j))+double(imagenR(i,j+1)))/9);
         end
     end
+end
+function valorCentral = calcCentral(array)
+    B = sort(array);
+    valorCentral = B(5);
 
+end
+
+function matrizRuidosos = detectPRuidoso(imagenR,alto,ancho)
+    matrizRuidosos = zeros(2,1);
+    for i =2:alto
+        for j=2:ancho
+            media=round((double(imagenR(i,j))+double(imagenR(i+1,j+1))+double(imagenR(i-1,j-1))+double(imagenR(i-1,j))+double(imagenR(i-1,j+1))+double(imagenR(i,j-1))+double(imagenR(i+1,j-1))+double(imagenR(i+1,j))+double(imagenR(i,j+1)))/9);
+            if imagenR(i,j) > media+50 || imagenR(i,j) <media-50
+                matrizRuidosos = [matrizRuidosos [i;j]];
+            end
+        end
+    end
 end

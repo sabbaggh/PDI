88df69fecc16245dfd078186165cca0f0b1f2901
sabbaggh <137013454+sabbaggh@users.noreply.github.com>
2023-11-21 16:06:54 -0600

    Add files via upload

--- practica10N.m
+++ practica10N.m
@@ -14,17 +14,17 @@ fprintf('Se modifico el bit %d',bitError)
 
 function bitCambiado = cmabiarBit(cadena,num)
     longitud = strlength(cadena);
-    bitCambiado = '';
-    cadena(1)
+    bitCambiado = zeros(1,longitud);
+    bitCambiado = string(bitCambiado);
     for i = 1:longitud
         if i == num
             if cadena(i) == "1"
-                bitCambiado = bitCambiado+"0";
+                bitCambiado(i) = "0";
             else
-                bitCambiado = bitCambiado+"1";
+                bitCambiado(i) = "1";
             end
         else
-            bitCambiado = bitCambiado + cadena(i);
+            bitCambiado(i) = cadena(i);
         end
 
     end
@@ -38,3 +38,76 @@ function bitError = encontrarBitCambiado(cadenaOrg, cadenaNuev)
         end
     end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
